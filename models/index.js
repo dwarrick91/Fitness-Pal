70c2f525bc7adebf9cd232548e27a73eb0907b6f
Owen Foreman <owen4man@gmail.com>
2022-04-25 17:08:34 -0400

    user/workout models setup

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Workout = require('./Workout');
 
-User.hasMany(Project, {
+User.hasMany(Workout, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Workout.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Workout };
