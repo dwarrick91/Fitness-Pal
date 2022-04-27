0e5398581a48e56dc74d052cc75d2bc9b259e862
Owen Foreman <owen4man@gmail.com>
2022-04-27 11:46:53 -0400

    date and add info fix and buttons fix

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const workoutRoute = require('./workoutRoute');
 
 router.use('/', homeRoutes);
-router.use('/', workoutRoute);
 router.use('/api', apiRoutes);
 
 module.exports = router;
