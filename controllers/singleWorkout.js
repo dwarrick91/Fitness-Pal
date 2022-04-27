880ee79a1b5011af2e73fb044a923140939846d6
Owen Foreman <owen4man@gmail.com>
2022-04-27 15:56:13 -0400

    view single workout when clicking on calender

--- controllers/singleWorkout.js
+++ controllers/singleWorkout.js
@@ -4,7 +4,9 @@ const withAuth = require('../utils/auth');
 
 //view specific workouts in calender
 router.get('/workout/:id', withAuth, async (req, res) => {
+  console.log("hit single workout");
   try {
+    console.log(req.params.id);
     const singleWorkoutData = await Workout.findByPk(req.params.id, {
       include: [
         {
@@ -15,12 +17,27 @@ router.get('/workout/:id', withAuth, async (req, res) => {
     });
 
     const workout = singleWorkoutData.get({ plain: true });
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Workout }],
+    });
 
+    const user = userData.get({ plain: true });
+
+    console.log(user);
+    const workoutData = await Workout.findAll({
+      where: {
+        user_id: user.id,
+      },
+      raw: true,
+    });
     res.render('singleWorkout', {
       ...workout,
+      workoutData,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
